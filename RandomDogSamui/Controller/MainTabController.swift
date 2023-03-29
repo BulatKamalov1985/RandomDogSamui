e725bce79928a7cd6e33b5433d23133dcdbbbb59
Bulat Kamalov <kamalovmarket@gmail.com>
2023-03-29 16:19:57 +0700

    madeTabbarControllerWithoutStoryboard

--- RandomDogSamui/Controller/MainTabController.swift
+++ RandomDogSamui/Controller/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  RandomDogSamui
 //
 //  Created by Bulat Kamalov on 29.03.2023.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+
     }
-
-
+    
 }
-
